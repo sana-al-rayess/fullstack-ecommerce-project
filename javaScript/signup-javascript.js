2fd248aea9c12c5f9ddf805fc71c0a199981ad72
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-03-04 21:57:50 +0200

    added script tp sign up

---
+++ javaScript/signup-javascript.js
@@ -0,0 +1,7 @@
+const firstNameInput = document.querySelector('#first-name');
+const lastNameInput = document.querySelector('#last-name');
+const userNameInput = document.querySelector('#user-name');
+const bDateInput = document.querySelector('#b-date');
+const emailInput = document.querySelector('#email');
+
+const passwordInput = document.querySelector('#password');
