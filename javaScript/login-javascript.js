10071d6f4f52e4d358f198b84a2f5d287c5f34ab
Sana Rayess <rayess.sana@gmail.com>
2023-03-05 17:29:42 +0200

    edited functions

--- javaScript/login-javascript.js
+++ javaScript/login-javascript.js
@@ -26,44 +26,12 @@ console.log(data);
 
 });
 
-let signup_btn = document.getElementById('signup');
+
 let signin_btn = document.getElementById('signin')
-signup_btn.addEventListener('click', signup);
+
 signin_btn.addEventListener('click', signin);
 
-function signup() {
 
-    let username = document.getElementById('username_signup').value;
-    let password = document.getElementById('password_signup').value;
-    let first_name = document.getElementById('first_signup').value;
-    let last_name = document.getElementById('last_signup').value;
-
-    let data = new FormData();
-    data.append('username', username);
-    data.append('password', password);
-    data.append('first_name', first_name);
-    data.append('last_name', last_name);
-
-    
-    axios({
-        "method": "post",
-        "url": "http://localhost/foodex_backend/signup.php",
-        "data": data
-    }).then((result) => {
-        console.log(result)
-        if (result.data.status == "success") {
-            alert("signed up")
-        }
-    }).catch((err) => {
-        console.error(err)
-    });
-
-    // axios.post("http://localhost/foodex_backend/signup.php",data).then(function(res){
-    //     console.log(res)
-    // }).catch(function(err){
-    //     console.error(err)
-    // })
-}
 
 function signin() {
     let username = document.getElementById('login-user-name').value;
