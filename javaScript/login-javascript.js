3becdb2d05e2725d9ccb6c78ec135f51176549d8
Sana Rayess <rayess.sana@gmail.com>
2023-03-05 17:24:34 +0200

    adde signup and login functions

--- javaScript/login-javascript.js
+++ javaScript/login-javascript.js
@@ -25,3 +25,60 @@ const data = JSON.stringify(loginData);
 console.log(data);
 
 });
+
+let signup_btn = document.getElementById('signup');
+let signin_btn = document.getElementById('signin')
+signup_btn.addEventListener('click', signup);
+signin_btn.addEventListener('click', signin);
+
+function signup() {
+
+    let username = document.getElementById('username_signup').value;
+    let password = document.getElementById('password_signup').value;
+    let first_name = document.getElementById('first_signup').value;
+    let last_name = document.getElementById('last_signup').value;
+
+    let data = new FormData();
+    data.append('username', username);
+    data.append('password', password);
+    data.append('first_name', first_name);
+    data.append('last_name', last_name);
+
+    
+    axios({
+        "method": "post",
+        "url": "http://localhost/foodex_backend/signup.php",
+        "data": data
+    }).then((result) => {
+        console.log(result)
+        if (result.data.status == "success") {
+            alert("signed up")
+        }
+    }).catch((err) => {
+        console.error(err)
+    });
+
+    // axios.post("http://localhost/foodex_backend/signup.php",data).then(function(res){
+    //     console.log(res)
+    // }).catch(function(err){
+    //     console.error(err)
+    // })
+}
+
+function signin() {
+    let username = document.getElementById('login-user-name').value;
+    let password = document.getElementById('login-password').value;
+    let data = new FormData();
+    data.append('username', username);
+    data.append('password', password);
+    axios.post('http://localhost/jewellery_backend/login.php', data).then(function (res) {
+        console.log(res.data)
+        // window.localStorage.setItem('user_id', res.data.user_id)
+        // window.sessionStorage.setItem('user_id', res.data.user_id)
+        console.log(window.sessionStorage.getItem('user_id'))
+
+    }).catch(function (err) {
+        console.log(err);
+    })
+
+}
--- javaScript/signup-javascript.js
+++ javaScript/signup-javascript.js
@@ -53,3 +53,39 @@ const userData = {
    
 
 });
+
+let signup_btn = document.getElementById('signup');
+signup_btn.addEventListener('click', signup);
+
+
+function signup() {
+
+    let username = document.getElementById('user-name').value;
+    let password = document.getElementById('password').value;
+    let first_name = document.getElementById('first-name').value;
+    let last_name = document.getElementById('last-name').value;
+    let email = document.getElementById('email').value;
+
+    let data = new FormData();
+    data.append('username', username);
+    data.append('password', password);
+    data.append('first_name', first_name);
+    data.append('last_name', last_name);
+    data.append('email', email);
+
+    
+    axios({
+        "method": "post",
+        "url": "http://localhost/jewellery_backend/signup.php",
+        "data": data
+    }).then((result) => {
+        console.log(result)
+        if (result.data.status == "success") {
+            alert("signed up")
+        }
+    }).catch((err) => {
+        console.error(err)
+    });
+
+   }
+
