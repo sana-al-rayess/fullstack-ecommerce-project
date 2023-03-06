6d1daaa4db33d875148b88aea66f855c54c3ae33
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-03-06 20:27:59 +0200

    rermoved comments

--- javaScript/favorites.js
+++ javaScript/favorites.js
@@ -1,31 +1,19 @@
 const productData = {};
 
-// Get all the item images
 const itemImages = document.querySelectorAll('.icon_fav');
 
-// Loop through each item image
 itemImages.forEach(itemImage => {
-    // Get the product data as a JavaScript object
     const product = JSON.parse(itemImage.dataset.product);
 
-    // Add the product data to the dictionary using the product ID as the key
     productData[product.id] = product;
 
-    // Add a click event listener to the item image
     itemImage.addEventListener('click', () => {
-        // Get the product ID from the data-product attribute of the image
         const productId = product.id;
 
-        // Get the product data from the dictionary using the product ID as the key
         const productDataItem = productData[productId];
 
-        // Do something with the product data, such as display it in the console
         console.log(productDataItem);
     });
 });
 
-// Display the productData dictionary in the console
 const productDataJSON = JSON.stringify(productData);
-
-// Display the productDataJSON string in the console
-//console.log(productDataJSON);
